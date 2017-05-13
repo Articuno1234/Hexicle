aa51245608bad442b32717a93a972f2f4e0802f1
Articuno1234 <artbothelp@gmail.com>
2017-05-13 10:14:20 +1000

    Hexicle info

---
+++ info.py
@@ -0,0 +1,10 @@
+print("===================\n"
+      "Hexicle⬢ 六方晶\n"     
+      "===================\n")
+print("Name:")
+print("Hexicle⬢ 六方晶#5117")
+print("\n")
+print("\n")
+print("Owner:")
+print("Articuno™&© | Hexicle Creator#9712")
+print("https://discordapp.com/channels/@me/284631314462670848")
